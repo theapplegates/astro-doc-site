5aca65251ecd141d35911cf7f4532a4582e50876
Paul Applegate <paul@theapplegates.com>
2022-06-29 17:16:09 +0000

    tests

--- src/config.ts
+++ src/config.ts
@@ -1,28 +1,29 @@
 export const SITE = {
-	title: 'Documentation',
+	title: 'Whats Wrong Now Dad?',
 	description: 'Your website description.',
 	defaultLanguage: 'en_US',
 };
 
 export const OPEN_GRAPH = {
 	image: {
-		src: 'https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true',
+		src: 'https://github.com/theapplegates/astro-doc-site/blob/main/assets/social/banner.jpg?raw=true',
 		alt:
 			'astro logo on a starry expanse of space,' +
 			' with a purple saturn-like planet floating in the right foreground',
 	},
-	twitter: 'astrodotbuild',
+	twitter: 'papplegate',
 };
 
 export const KNOWN_LANGUAGES = {
 	English: 'en',
 };
 
+
 // Uncomment this to add an "Edit this page" button to every page of documentation.
-// export const GITHUB_EDIT_URL = `https://github.com/withastro/astro/blob/main/docs/`;
+ export const GITHUB_EDIT_URL = `https://github.com/theapplegates/astro-doc-site/blob/main/docs/`;
 
 // Uncomment this to add an "Join our Community" button to every page of documentation.
-// export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
+ export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
 // Uncomment this to enable site search.
 // See "Algolia" section of the README for more information.
